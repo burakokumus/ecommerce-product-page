c82bc7b0b1877927ffd5a8256845b3a866032a5e
Burak Okumus <burakokumus98@gmail.com>
2024-04-10 13:30:16 +0300

    Add pp and logo to mobile navbar

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -15,19 +15,29 @@ const MobileNavbar = () => {
   const [sheetOpen, setSheetOpen] = useState(false);
 
   return (
-    <div className="md:hidden fixed top-0 flex flex-row pt-8 pl-8">
+    <div className="md:hidden fixed top-0 flex flex-row pt-8 pl-8 items-center justify-center gap-4">
       <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
         <SheetTrigger>
           <Image
             src="/images/icon-menu.svg"
             alt="menu"
             width={16}
-            height={15}
+            height={153}
             className="cursor-pointer"
           />
         </SheetTrigger>
         <SheetContent side="left">
           <div className="flex flex-col">
+
+            <Link href="/" className="mb-4">
+              <Image
+                src="/images/logo.svg"
+                alt="menu"
+                width={138}
+                height={20}
+                className="cursor-pointer"
+              />
+            </Link>
             {headerLinks.map((link) => (
               <Link
                 href={link.href}
@@ -40,6 +50,32 @@ const MobileNavbar = () => {
           </div>
         </SheetContent>
       </Sheet>
+
+      <Link href="/" className="mr-16">
+        <Image
+          src="/images/logo.svg"
+          alt="menu"
+          width={138}
+          height={20}
+          className="cursor-pointer"
+        />
+      </Link>
+      <div className="ml-auto flex flex-row items-center">
+        <Image
+          src="/images/icon-cart.svg"
+          alt="Cart"
+          width={20}
+          height={20}
+          className="ml-12 mr-4 cursor-pointer"
+        />
+        <Image
+          src="/images/image-avatar.png"
+          alt="avatar"
+          width={32}
+          height={32}
+          className="cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
