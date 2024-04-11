8f4ca27da8e0e033935d7d9eb7a8284ffe9cac17
Burak Okumus <burakokumus98@gmail.com>
2024-04-11 17:39:05 +0300

    Separate carousel component

---
+++ src/components/product/ProductCarousel.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { cn, productList } from "@/lib/utils";
+import Image from "next/image";
+import React, { useState } from "react";
+
+export interface ProductCarouselProps {
+  onClick?: () => void;
+}
+
+const ProductCarousel = ({ onClick }: ProductCarouselProps) => {
+  const [selectedImage, setSelectedImage] = useState(productList[0].img);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [products, setProducts] = useState(productList);
+  const [lightboxVisible, setLightboxVisible] = useState(false);
+
+  const handleSelect = (id: number) => {
+    const updatedProducts = productList.map((product) => {
+      if (id === product.id) {
+        setSelectedImage(product.img);
+        return { ...product, isActive: true };
+      }
+
+      return { ...product, isActive: false };
+    });
+
+    setCurrentIndex(id);
+    setProducts(updatedProducts);
+  };
+
+  return (
+    <div className="hidden md:flex md:flex-col">
+      <Image
+        src={selectedImage}
+        alt="Product image"
+        quality={100}
+        width={500}
+        height={500}
+        className="object-cover rounded-xl cursor-pointer"
+        onClick={onClick}
+      />
+      <div className="mt-8 flex flex-row justify-around px-4">
+        {products.map((product) => (
+          <Image
+            key={product.id}
+            src={product.thumbnail}
+            alt="thumbnail"
+            quality={100}
+            width={90}
+            height={90}
+            className={cn(
+              product.isActive
+                ? " opacity-50 border-orange-primary border-2"
+                : "",
+              "rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out"
+            )}
+            onClick={() => handleSelect(product.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCarousel;
--- src/components/product/ProductImage.tsx
+++ src/components/product/ProductImage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ProductCarousel from "./ProductCarousel";
 import { cn, productList } from "@/lib/utils";
 import Image from "next/image";
 import { useState } from "react";
@@ -74,68 +75,10 @@ const ProductImage = () => {
           <span className="sr-only">back button</span>
         </Button>
       </div>
-      <div className="hidden md:flex md:flex-col">
-        <Image
-          src={selectedImage}
-          alt="Product image"
-          quality={100}
-          width={500}
-          height={500}
-          className="object-cover rounded-xl cursor-pointer"
-          onClick={() => setLightboxVisible(true)}
-        />
-        <div className="mt-8 flex flex-row justify-around px-4">
-          {products.map((product) => (
-            <Image
-              key={product.id}
-              src={product.thumbnail}
-              alt="thumbnail"
-              quality={100}
-              width={90}
-              height={90}
-              className={cn(
-                product.isActive
-                  ? " opacity-50 border-orange-primary border-2"
-                  : "",
-                "rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out"
-              )}
-              onClick={() => handleSelect(product.id)}
-            />
-          ))}
-        </div>
-      </div>
+      <ProductCarousel onClick={() => setLightboxVisible(true)} />
       {lightboxVisible && (
         <Lightbox handleClose={() => setLightboxVisible(false)}>
-          <div className="hidden md:flex md:flex-col">
-            <Image
-              src={selectedImage}
-              alt="Product image"
-              quality={100}
-              width={500}
-              height={500}
-              className="object-cover rounded-xl cursor-pointer"
-              onClick={() => setLightboxVisible(true)}
-            />
-            <div className="mt-8 flex flex-row justify-around px-4">
-              {products.map((product) => (
-                <Image
-                  key={product.id}
-                  src={product.thumbnail}
-                  alt="thumbnail"
-                  quality={100}
-                  width={90}
-                  height={90}
-                  className={cn(
-                    product.isActive
-                      ? " opacity-50 border-orange-primary border-2"
-                      : "",
-                    "rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out"
-                  )}
-                  onClick={() => handleSelect(product.id)}
-                />
-              ))}
-            </div>
-          </div>
+          <ProductCarousel />
         </Lightbox>
       )}
     </>
