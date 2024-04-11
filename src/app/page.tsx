e259c2aca9aa432522e2702ea1e391e6f1f68d9c
Burak Okumus <burakokumus98@gmail.com>
2024-04-11 11:51:11 +0300

    Add product thumbnails

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { thumbnails } from "@/lib/utils";
 import Image from "next/image";
 
 import Counter from "@/components/Counter";
@@ -16,7 +17,7 @@ export default function Home() {
             className="object-cover"
           />
         </div>
-        <div className="hidden md:block">
+        <div className="hidden md:flex md:flex-col">
           <Image
             src="/images/image-product-1.jpg"
             alt="Product image"
@@ -25,6 +26,19 @@ export default function Home() {
             height={500}
             className="object-cover rounded-xl"
           />
+          <div className="mt-8 flex flex-row justify-around px-4">
+            {thumbnails.map((thumbnail) => (
+              <Image
+                key={thumbnail}
+                src={thumbnail}
+                alt="thumbnail"
+                quality={100}
+                width={90}
+                height={90}
+                className="rounded-lg cursor-pointer hover:shadow-lg transition-shadow duration-300 ease-in-out"
+              />
+            ))}
+          </div>
         </div>
         <div className="mt-24 px-8 md:px-16 md:mt-0">
           <p className="text-orange-primary font-semibold uppercase text-xs tracking-widest mb-4">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,10 @@ export const headerLinks = [
   { href: "/", label: "About" },
   { href: "/", label: "Contact" },
 ];
+
+export const thumbnails = [
+  "/images/image-product-1-thumbnail.jpg",
+  "/images/image-product-2-thumbnail.jpg",
+  "/images/image-product-3-thumbnail.jpg",
+  "/images/image-product-4-thumbnail.jpg",
+];
