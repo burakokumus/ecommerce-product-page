e441c33043693ef29b1db1fd36b39f33e6c2947b
Burak Okumus <burakokumus98@gmail.com>
2024-04-11 13:14:11 +0300

    Add slider buttons to mobile

--- src/components/product/ProductImage.tsx
+++ src/components/product/ProductImage.tsx
@@ -4,6 +4,8 @@ import { cn, productList } from "@/lib/utils";
 import Image from "next/image";
 import { useState } from "react";
 
+import { Button } from "../ui/button";
+
 const ProductImage = () => {
   const [selectedImage, setSelectedImage] = useState(productList[0].img);
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -25,7 +27,7 @@ const ProductImage = () => {
 
   return (
     <>
-      <div className="relative w-full h-[33vh] top-12 overflow-hidden md:hidden">
+      <div className="relative w-full h-[34vh] top-12 overflow-hidden md:hidden">
         <Image
           src={selectedImage}
           alt="Product image"
@@ -33,6 +35,42 @@ const ProductImage = () => {
           fill={true}
           className="object-cover"
         />
+        <Button
+          className="absolute z-50 top-[17vh] bg-white hover:bg-blue-light-grayish w-10 aspect-square left-3 grid place-items-center rounded-full"
+          onClick={(e: any) => {
+            console.log("Back");
+            if (currentIndex > 1) {
+              handleSelect(currentIndex - 1);
+            }
+          }}
+        >
+          <Image
+            src={"/images/icon-next.svg"}
+            alt=""
+            height={18}
+            width={13}
+            className="rotate-180"
+          />
+          <span className="sr-only">back button</span>
+        </Button>
+        <Button
+          className="absolute z-50 top-[17vh] bg-white hover:bg-blue-light-grayish w-10 aspect-square right-3 grid place-items-center rounded-full"
+          onClick={(e: any) => {
+            console.log("Forward");
+            if (currentIndex < productList.length) {
+              handleSelect(currentIndex + 1);
+            }
+          }}
+        >
+          <Image
+            src={"/images/icon-next.svg"}
+            alt=""
+            height={18}
+            width={13}
+            className=""
+          />
+          <span className="sr-only">back button</span>
+        </Button>
       </div>
       <div className="hidden md:flex md:flex-col">
         <Image
